46602b0af34b27364a3650101339b684b9646aa4
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-11-11 14:43:48 -0300

    atribuiçao de valores simples

--- aula_1/treino.py
+++ aula_1/treino.py
@@ -1,29 +1,15 @@
-a = (1,2,3,4,5)
-b = (22,55)
+# atribuiçao :
+
+a = 22
+
+a+=45
+
+print(a)
+
+# saida : 67,atribuimos um valor em especifico para a variavel "a" que tinha valor como 22
 
 
+b = 156
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+b-=33
+print(b)
