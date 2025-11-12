d49277bd3e0dee27557ec77a517a997a5686af06
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-11-12 18:39:26 -0300

    funçoes especificas

---
+++ aula_041.py
@@ -0,0 +1,32 @@
+# operador SUM :
+
+lista = [1,2,3]
+lista_b = [3,4,5]
+
+print(sum(lista + lista_b))
+
+a = [23,45,66]
+b = [4,23,44]
+
+print(sum(a+b))
+
+
+# conversao de tipos :
+
+a = 3
+b ="2"
+
+print(a + int(b))
+
+c = "2"
+d = "1"
+
+print(int(c)+int(d))
+
+z = "23"
+y = 12
+b = "56"
+h = 23
+
+print(int(b)+int(z))
+print(y + h)
--- teste.py
+++ teste.py
@@ -7,13 +7,23 @@
 
 # print(n_1 is n_3 and n_5 is n_2 and n_4 is n_3
 
-lista = [1,2,3,4,5]
-lista_b = lista
-lista_c = [1,2,3,4,5]
+# lista = [1,2,3,4,5]
+# lista_b = lista
+# lista_c = [1,2,3,4,5]
 
-print(lista is lista_b)
-print(lista_b is lista_c)
+# print(lista is lista_b)
+# print(lista_b is lista_c)
 
+
+a = 3
+b ="2"
+
+print(a + int(b))
+
+c = "2"
+d = "1"
+
+print(int(c)+int(d))
 
 
 
