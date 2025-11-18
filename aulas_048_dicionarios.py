2590c74d07d5a3f5a5f1bcc3e0dc87cd273618c8
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-11-18 17:21:26 -0300

    dicionarios

---
+++ aulas_048_dicionarios.py
@@ -0,0 +1,21 @@
+dicionario = {"nome":"vitor","cidade":"carapicuiba"}
+
+for x,i in enumerate(dicionario):
+    print("valor de x: ",x, "valor de i:", i)
+
+dicionario_a = {"nome":"vitor","cidade":"carapicuiba","municipio":"osasco"}
+print(len(dicionario_a))
+print(dicionario_a["nome"])
+print(dicionario_a["municipio"])
+print(dicionario_a["cidade"])
+
+for letras in dicionario_a.items():
+    print(f"informaçoes do usuario : {letras}")
+    
+
+vitor = {"idade":16,"cidade":"carapicuiba"}
+
+# forma de acessa valores mais facil :
+print(vitor.keys())
+print(vitor.values())
+print(vitor.items())
