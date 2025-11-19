221baa1a63da52dcd66c213e4fa526f36f35358a
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-11-19 13:24:40 -0300

    conjuntos

--- aula_050_conjuntos.py
+++ aula_050_conjuntos.py
@@ -1,4 +1,8 @@
+a = {1,2,3}
+print(type(a))
+a = set("vitor")
+print(a)
 
-
-
+b = set("joao")
+print(b)
 
--- teste.py
+++ teste.py
@@ -1,7 +1,4 @@
-pi = 3.14
-raio = 25
 
-soma = pi*raio
+    
 
 
-print("a area do circulo e igual a :",soma, "perfeito")
