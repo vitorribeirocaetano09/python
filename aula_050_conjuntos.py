ed4d71b4b360543c465070a54bd0ca5dc573479e
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-11-21 11:15:00 -0300

    conjuntos 02

--- aula_050_conjuntos.py
+++ aula_050_conjuntos.py
@@ -1,8 +1,12 @@
-a = {1,2,3}
-print(type(a))
-a = set("vitor")
-print(a)
+a = {1,2,3,4}
+for i in a:
+    print("valores do conjunto  : ",i)
 
-b = set("joao")
+
+a = {2,3,4,5,6,"10"}
+for i in range (11,20):
+    print(i)
+    
+b = {33,4,5}
+b.remove(33)
 print(b)
-
