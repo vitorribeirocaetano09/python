11756a3102b01230514cc2d9772494c8a2efe04e
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-12-01 16:20:05 -0300

    while teste 02

--- while.py
+++ while.py
@@ -1,33 +1,14 @@
-i = 0
+# entrada = False
 
-while i <2:
-    print(f"o comando começa com : {i}")
-    i+=1
-if i <= 2:
-    print("codigo muito baixo")
-else:
-    print("codigo finalizado com sucesso")
-
-
-
-print("ola usuario novo sera que pode assinar isso? :")
-
-lista = ["vitor",1234]
-
-while True:
-    nome = input("digite para validar por favor  :")
+# while not entrada:
+#     tentativa = input("digite uma palavra chave para entrar :")
     
-    senha = int(input("digite a senha para validar :"))
+#     if tentativa == "sair":
+#         print("saindo do loop ")
+#         break
     
-    if nome and senha in lista :
-        print(f"usuario encontrado com sucesso,pode continuar")
-        break
-        
-    elif nome == lista:
-        print("nome correto mais senha nao encontrada!")
+#     elif tentativa == "acessar":
+#         print("acessando dados")
     
-    elif senha == lista:
-        print("senha correta mais nome nao encontrado")
-        
-    else:
-        print("usuario nao encontrado")
+#     else:
+#         print("fim")
