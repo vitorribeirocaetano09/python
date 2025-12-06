f96694d37e76b23ae12e701b7c07dce86165a630
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-12-06 17:50:53 -0300

    teste while 06

--- aula_teste_02.py
+++ aula_teste_02.py
@@ -1,14 +1,16 @@
-print("-----> lista de compras a fazer :")
-
-lista_compras = []
-
+lista = [22,33]
 while True:
-    item = input("digite um item para sair da lista : ")
-    break
-
-lista_compras.append(item)
     
-for letras in lista_compras:
-     print(f"sua lista de compras atual ---> : {lista_compras}")
-     
-     print("---->lista encerrada")
+    numero_digitar = int(input("digite um numero : "))
+    if numero_digitar >= 10:
+        somando = sum(lista)
+        print(f"valor : {numero_digitar},esta de acordo com caso :")
+        print(f"soma ocorrida com sucesso ------> : {somando}")
+        break
+    
+    elif numero_digitar == 0:
+        print("saindo do programa")
+        
+    else:
+        print(f"numero nao encontrado : {numero_digitar}")
+        print("tente novamente em ------")
