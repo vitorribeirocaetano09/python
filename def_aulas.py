1799b88d5e7a1bf7117c10c20081dd50198d8419
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2025-12-17 13:26:39 -0300

    def teste 02

--- def_aulas.py
+++ def_aulas.py
@@ -1,26 +1,17 @@
-notas = [8,9,4,6,5,5,6]
-media = 6
+elo_estimado = 1500
+elo_real = 2200
+esperado = 1000
 
-def calcular_notas():
-    somar = sum(notas)
-    subtrair = somar /media
-    return subtrair
+def calcular_rating():
+    corte = (elo_estimado - esperado)/2
+    comparaçao = (corte - elo_real)
+    return comparaçao
 
-media_final = calcular_notas
-print(f"notas disponiveis ------>")
+realidade_rating = (esperado - calcular_rating())
+print(f"rating esperado no final igual a : {realidade_rating}")
 
-if media_final == 20:
-    print(f"a nota atingiu o esperado : {media_final}")
+if realidade_rating >= 2556:
+    print("meus parabens vc e um grao mestre : ------>")
 
 else:
-    print(f"nota insulficiente")
-
-
-
-    
-
-
-
-
-
-
+    print("vc e um candidato a mestre ainda :  ----->")
