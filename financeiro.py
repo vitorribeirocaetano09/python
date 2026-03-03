8eac1831f33d511a96fe6ea48e1c2a305e03a8c9
Vitor Ribeiro Caetano <vitorvrcaetano@gmail.com>
2026-03-03 19:20:14 -0300

    aula 82

--- financeiro.py
+++ financeiro.py
@@ -1,59 +1,10 @@
-# capital = int(input("digite seu capital aqui --> :"))
-# taxa_mensal = float(input("digite uma taxa --> :"))
-# periodo = int(input("periodo estimado : "))
-
-# if __name__ == "__main__":
-#     calculo_simples = (capital*taxa_mensal*periodo)
-#     montante = capital + calculo_simples
-    
-#     for contas in (calculo_simples,montante):
-#         print(f"situaçao atual -----> : ",contas)
-    
-#     if contas >= int(1250):
-#         soma_nova = montante + 100
-#         print("seu novo montante em cima do valor do banco e igual a :",soma_nova)
-    
-#     else:
-#         soma_subtraida = montante-float(2.55)
-#         print("montante alterado,verifique com a agencia bancaria : ",soma_subtraida)
-        
-      
-# taxa = 0.05
-
-# conversao = (taxa)/100
-
-# print(f"taxa : {taxa}, conversao igual a : {conversao}")
+def notas_alunos(notas):
+    digitar = float(input("digite sua nota"))
+    if notas >= 7:
+        return "nota acima da media"
+    if notas >= 5:
+        return "nota meidiana"
 
 
-# rendimento_juros_simples = 200.25
 
-# print(f"rendimento sobre o juros simples ---> : {rendimento_juros_simples}")
 
-# print("novo rendimento em seguida,composto")
-
-# investimento_seguido = float(input("digite um valor para investir : "))
-# periodo = int(input("tempo de rendimento ---> : "))
-
-# if __name__=="__main__":
-#     b = float(investimento_seguido*periodo)
-#     a = (b+rendimento_juros_simples)/100
-#     print(f"novo investimento : {a} ")
-
-def calcular_notas(valor):
-    notas = float(valor)
-    
-    if notas == 10.5:
-        print("nota maxima muito bem")
-    
-    
-    
-    
-    
-    
-    
-    
-      
-     
-
-      
-      
